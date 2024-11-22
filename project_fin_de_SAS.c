43212e445f1558e9ee8c41a3b756aa4932b77fc3
SamiChafik <sami.chafik00@gmail.com>
2024-11-22 16:46:26 +0100

    added delete function and some fixes

--- project_fin_de_SAS.c
+++ project_fin_de_SAS.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 typedef struct {
-    int hour,day,month;
+    int day,month,year;
 } date;
 typedef struct {
     char title[100],discreption[400];
@@ -24,23 +24,29 @@ void add(task t[]) {
     scanf (" %[^\n]", t[count].title);
     printf ("discription : ");
     scanf (" %[^\n]", t[count].discreption);
-    printf ("priority\n1-high\n2-low)\nchoose one : ");
-    scanf ("%d", &t[count].prio);
+    do {
+        printf ("priority\n1-high\n2-low\nchoose one : ");
+        scanf ("%d", &t[count].prio);
+    } while (t[count].prio != 1 && t[count].prio != 2);
     count++;
     printf ("=== Task added ! ===\n");
 }
 void show(task t[]) {
     int i;
-    printf ("<= List of tasks =>\n");
 
-    for (i = 0; i < count; i++) {
-        printf ("=== Task %d ===\n", i+1);
-        printf ("Tilte : %s\n", t[i].title);
-        printf ("description : %s\n", t[i].discreption);
-        printf ("prio : ");
-        if (t[i].prio == 1) {
-            printf ("high\n");
-        } else printf ("low\n");
+    if (count == 0) { 
+        printf ("<= no tasks stored ! =>\n");
+    } else {
+        for (i = 0; i < count; i++) {
+            printf ("<= List of tasks =>\n");
+            printf ("=== Task %d ===\n", i+1);
+            printf ("Tilte : %s\n", t[i].title);
+            printf ("description : %s\n", t[i].discreption);
+            printf ("prio : ");
+            if (t[i].prio == 1) {
+                printf ("high\n");
+            } else printf ("low\n");
+        }
     }
 
 }
@@ -59,12 +65,32 @@ void edit(task t[]) {
     scanf (" %[^\n]", t[num].title);
     printf ("New description : ");
     scanf (" %[^\n]", t[num].discreption);
-    printf ("Change priority (1-high or 2-low) : ");
-    scanf ("%d", &t[num].prio);
+    do {
+        printf ("priority\n1-high\n2-low\nchoose one : ");
+        scanf ("%d", &t[count].prio);
+    } while (t[count].prio != 1 && t[count].prio != 2);
     printf ("<= Task changed successfully ! =>\n");
 }
-void delete() {
-    printf ("test 4\n");
+void delete(task t[]) {
+    int ch,i,num;
+    printf ("1-delete one tastk\n2-delete all tasks\n");
+    scanf ("%d", &ch);
+    if (ch == 0 || ch > count) {
+        printf ("this task does not exist !\n");
+    }
+    if (ch == 1) {
+        printf ("entre the task number you want to delete : ");
+        scanf ("%d", &num);
+        num--;
+        for (i=num; i < count; i++) {
+            t[i] = t[i+1];
+        }
+        printf ("\n<= task deleted successfuly ! =>\n");
+        count--;
+    } else if (ch == 2) {
+        count = 0;
+        printf ("<= all tasks deleted succefully ! =>\n")
+    } else printf ("invalid choice, try again !\n");
 }
 void sort() {
     printf ("test 5\n");
@@ -75,13 +101,7 @@ int main() {
     task t[50];
 
     do {
-        printf ("=== Menu ===\n");
-        printf ("1-Add new task\n");
-        printf ("2-Show tasks\n");
-        printf ("3-Edit a task\n");
-        printf ("4-Delete a task\n");
-        printf ("5-Sort tasks\n");
-        printf ("0-Exit\n");
+        printf ("=== Menu ===\n1-Add a new task\n2-Show tasks\n3-Edit a task\n4-Delete a task\n5-Sort tasks\n0-Exit\n");
 
         printf (" ==> entre your choice : ");
         scanf ("%d", &ch);
@@ -100,7 +120,7 @@ int main() {
             edit(t);
             break;
             case 4 :
-            delete();
+            delete(t);
             break;
             case 5 :
             sort();
