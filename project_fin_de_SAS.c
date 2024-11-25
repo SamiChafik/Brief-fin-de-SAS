889293b9b7b3b783270648050dc88f4f76f230d1
SamiChafik <sami.chafik00@gmail.com>
2024-11-25 17:25:54 +0100

    added more stuff to my code

--- project_fin_de_SAS.c
+++ project_fin_de_SAS.c
@@ -8,6 +8,7 @@ typedef struct {
 typedef struct {
     char title[100],discreption[400];
     int prio;
+    int completion;
     date date;
 } task;
 
@@ -16,17 +17,17 @@ int count = 0;
 void add(task t[]) {
     int ch;
     if (count >= 50) {
-        printf ("you reach the limit of tasks you can add.\n");
+        printf ("\nyou reach the limit of tasks you can add.\n");
         return; 
     }
 
     while (1) {
         do {
-            printf ("1-Creat new task\n2-Return to main menut\n=> ");
+            printf ("\n1-Creat new task\n2-Return to main menu\n=> ");
             scanf ("%d", &ch);
 
             if (ch == 1) {
-                printf ("=== Task %d ===\n", count+1);
+                printf ("\n=== Task %d ===\n", count+1);
                 printf ("title : ");
                 scanf (" %[^\n]", t[count].title);
                 printf ("discription : ");
@@ -67,115 +68,277 @@ void add(task t[]) {
                 printf ("=== Task added ! ===\n");
             } else if (ch == 2) {
                 return;
-            } else printf ("invalid choice, try again !\n");
+            } else printf ("\ninvalid choice, try again !\n");
         } while (ch < 1 || ch > 2);
     }
 }
+
 void show(task t[]) {
     int i,ch;
-    printf ("1-Show all tasks\n2-Show high priority tasks\n3-Show low priority tasks\n4-Return to main menu\n=> ");
-    scanf ("%d", &ch);
+    int d = 0;
 
-    if (ch == 1) {
-        if (count == 0) { 
-            printf ("<= no tasks stored ! =>\n");
-
-        
-        } else {
-            for (i = 0; i < count; i++) {
-                printf ("<= List of tasks =>\n");
-                printf ("=== Task %d ===\n", i+1);
-                printf ("Tilte : %s\n", t[i].title);
-                printf ("description : %s\n", t[i].discreption);
-                printf ("prio : ");
-                if (t[i].prio == 1) {
-                    printf ("high\n");
-                } else printf ("low\n");
-                printf ("the deadline date for this task is : %d / %d / %d \n", t[i].date.day, t[i].date.month, t[i].date.year);
-            }
-        }
-    } else if (ch == 4) {
+    if (count == 0) { 
+        printf ("\n<= no tasks stored ! =>\n");
         return;
     }
+
+    while (1) {
+        printf ("\n1-Show all tasks\n2-Show high priority tasks\n3-Show low priority tasks\n4-Return to main menu\n=> ");
+        scanf ("%d", &ch);
+
+        for (i = 0; i < count; i++) {
+            if ((ch == 2 && t[i].prio != 1 ) || (ch == 3 && t[i].prio != 2)) {
+                continue;
+            }
+            
+            printf ("\n=== Task %d ===\n", i+1);
+            printf ("Title : %s\n", t[i].title);
+            printf ("description : %s\n", t[i].discreption);
+            printf ("prio : ");
+            if (t[i].prio == 1) {
+            printf ("high\n");
+            } else printf ("low\n");
+            printf ("the deadline date for this task is : %d / %d / %d \n", t[i].date.day, t[i].date.month, t[i].date.year);
+
+            printf ("=========\n");
+                
+            d = 1;
+            
+        }
+
+        if (d == 0) {
+            printf ("\nNo task with this priority was found !\n");
+        }
+
+        if (ch == 6) {
+            return;
+        }
+    }
 }
+
 void edit(task t[]) {
-    int num,ch;
+    int num,ch,ch2,ch3;
+
+    if (count == 0) { 
+        printf ("\n<= no tasks stored ! =>\n");
+        return;
+    }
+
     while (1) {
         do {
-            printf ("1-Edit existing task\n2-Return to main menu\n=> ");
+            printf ("\n1-Edit existing task\n2-Return to main menu\n=> ");
             scanf ("%d", &ch);
 
             if (ch == 1) {
-                printf ("enter the task number you want to edit : ");
-                scanf ("%d", &num);
 
-                if (num < 0 || num > count) {
-                    printf ("task does not exist !\n");
-                    return;
+                printf ("\n1-Edit all\n2-Edit title\n3-Edit discription\n4-Edit priority\n5-Edit date\n6-Return\n=> ");
+                scanf ("%d", &ch2);
+                switch (ch2) {
+                    
+                    case 1 :
+                    printf ("enter the task number you want to edit : ");
+                    scanf ("%d", &num);
+
+                    if (num < 0 || num > count) {
+                        printf ("\ntask does not exist !\n");
+                        return;
+                    }
+
+                    num--;
+
+                    printf ("\n=== Task %d ===\n", num+1);
+                    printf ("New tilte : ");
+                    scanf (" %[^\n]", t[num].title);
+                    printf ("New description : ");
+                    scanf (" %[^\n]", t[num].discreption);
+
+                    do {
+                        printf ("priority\n1-high\n2-low\nchoose one : ");
+                        scanf ("%d", &t[count].prio);
+                    } while (t[count].prio != 1 && t[count].prio != 2);
+
+                    printf ("Enter new deadline date\n");
+
+                    do {
+                        printf ("day (1-31) : ");
+                        scanf ("%d", &t[num].date.day);
+                        if (t[num].date.day < 1 || t[num].date.day > 31) {
+                            printf ("day most be between 1 and 31\n");
+                        }
+                    } while (t[num].date.day < 1 || t[num].date.day > 31);
+
+                    do {
+                        printf ("month (1-12) : ");
+                        scanf ("%d", &t[num].date.month);
+                        if (t[num].date.month < 1 || t[num].date.month > 12) {
+                            printf ("month most be between 1 and 12\n");
+                        }
+                    } while (t[num].date.month < 1 || t[num].date.month > 12);
+
+                    do {
+                        printf ("year (2024 or above) : ");
+                        scanf ("%d", &t[num].date.year);
+
+                        if (t[num].date.year < 2024) {
+                            printf ("the year most be 2024 or above\n");
+                        }
+                    } while (t[num].date.year < 2024 );
+
+                    printf ("\n<= Task changed successfully ! =>\n");
+                    break;
+
+                    case 2 :
+                    printf ("enter the task number you want to edit : ");
+                    scanf ("%d", &num);
+
+                    if (num < 0 || num > count) {
+                        printf ("\ntask does not exist !\n");
+                        return;
+                    }
+
+                    num--;
+
+                    printf ("\n=== Task %d ===\n", num+1);
+                    printf ("New tilte : ");
+                    scanf (" %[^\n]", t[num].title);
+                    break;
+
+                    case 3 :
+                    printf ("enter the task number you want to edit : ");
+                    scanf ("%d", &num);
+
+                    if (num < 0 || num > count) {
+                        printf ("\ntask does not exist !\n");
+                        return;
+                    }
+
+                    num--;
+
+                    printf ("\n=== Task %d ===\n", num+1);
+                    printf ("New discription : ");
+                    scanf (" %[^\n]", t[num].discreption);
+                    break;
+
+                    case 4 :
+                    printf ("enter the task number you want to edit : ");
+                    scanf ("%d", &num);
+
+                    if (num < 0 || num > count) {
+                        printf ("\ntask does not exist !\n");
+                        return;
+                    }
+
+                    num--;
+
+                    printf ("\n=== Task %d ===\n", num+1);
+                    do {
+                        printf ("priority\n1-high\n2-low\nchoose one : ");
+                        scanf ("%d", &t[num].prio);
+                    } while (t[num].prio != 1 && t[num].prio != 2);
+                    break;
+
+                    case 5 :
+                    printf ("\nEdit dead line date\n1-day\n2-month\n3-year\n4-Return\n=> ");
+                    scanf ("%d", &ch3);
+                    switch (ch3) {
+
+                        case 1 :
+                        printf ("enter the task number you want to edit : ");
+                        scanf ("%d", &num);
+
+                        if (num < 0 || num > count) {
+                            printf ("\ntask does not exist !\n");
+                            return;
+                        }
+
+                        num--;
+
+                        printf ("\n=== Task %d ===\n", num+1);
+                        do {
+                        printf ("day (1-31) : ");
+                        scanf ("%d", &t[num].date.day);
+                        if (t[num].date.day < 1 || t[num].date.day > 31) {
+                            printf ("day most be between 1 and 31\n");
+                        }
+                        } while (t[num].date.day < 1 || t[num].date.day > 31);
+                        break;
+
+                        case 2 :
+                        printf ("enter the task number you want to edit : ");
+                        scanf ("%d", &num);
+
+                        if (num < 0 || num > count) {
+                            printf ("\ntask does not exist !\n");
+                            return;
+                        }
+
+                        num--;
+
+                        printf ("\n=== Task %d ===\n", num+1);
+                        do {
+                        printf ("month (1-12) : ");
+                        scanf ("%d", &t[num].date.month);
+                        if (t[num].date.month < 1 || t[num].date.month > 12) {
+                            printf ("month most be between 1 and 12\n");
+                        }
+                        } while (t[num].date.month < 1 || t[num].date.month > 12);
+                        break;
+
+                        case 3 :
+                        printf ("enter the task number you want to edit : ");
+                        scanf ("%d", &num);
+
+                        if (num < 0 || num > count) {
+                            printf ("\ntask does not exist !\n");
+                            return;
+                        }
+
+                        num--;
+
+                        printf ("\n=== Task %d ===\n", num+1);
+                        do {
+                        printf ("year (2024 or above) : ");
+                        scanf ("%d", &t[num].date.year);
+
+                        if (t[num].date.year < 2024) {
+                            printf ("the year most be 2024 or above\n");
+                        }
+                        } while (t[num].date.year < 2024 );
+                        break;
+
+                        case 4 :
+                        break;
+                    }
+                    case 6 :
+                    break;
                 }
-
-                num--;
-
-                printf ("=== Task %d ===\n", num+1);
-                printf ("New tilte : ");
-                scanf (" %[^\n]", t[num].title);
-                printf ("New description : ");
-                scanf (" %[^\n]", t[num].discreption);
-
-                do {
-                    printf ("priority\n1-high\n2-low\nchoose one : ");
-                    scanf ("%d", &t[count].prio);
-                } while (t[count].prio != 1 && t[count].prio != 2);
-
-                printf ("Enter new deadline date\n");
-
-                do {
-                    printf ("day (1-31) : ");
-                    scanf ("%d", &t[num].date.day);
-                    if (t[num].date.day < 1 || t[num].date.day > 31) {
-                        printf ("day most be between 1 and 31\n");
-                    }
-                } while (t[num].date.day < 1 || t[num].date.day > 31);
-
-                do {
-                    printf ("month (1-12) : ");
-                    scanf ("%d", &t[num].date.month);
-                    if (t[num].date.month < 1 || t[num].date.month > 12) {
-                        printf ("month most be between 1 and 12\n");
-                    }
-                } while (t[num].date.month < 1 || t[num].date.month > 12);
-
-                do {
-                    printf ("year (2024 or above) : ");
-                    scanf ("%d", &t[num].date.year);
-
-                    if (t[num].date.year < 2024) {
-                        printf ("the year most be 2024 or above\n");
-                    }
-                } while (t[num].date.year < 2024 );
-
-                printf ("<= Task changed successfully ! =>\n");
             } else if (ch == 2) {
                 return;
-            } else printf ("invalid choice, try again !\n");
+            } else printf ("\ninvalid choice, try again !\n");
         } while (ch < 1 || ch > 2);
     }
 }
+
 void delete(task t[]) {
     int ch,i,num;
+    
+    if (count == 0) { 
+        printf ("\n<= no tasks stored ! =>\n");
+        return;
+    }
 
     while (1) {
         do {
-            printf ("1-Delete one tastk\n2-Delete all tasks\n3-Return to main menu\n=> ");
+            printf ("\n1-Delete one task\n2-Delete all tasks\n3-Return to main menu\n=> ");
             scanf ("%d", &ch);
 
             if (ch == 1) {
-                printf ("entre the task number you want to delete : ");
+                printf ("\nentre the task number you want to delete : ");
                 scanf ("%d", &num);
                 
                 if (num > count) {
-                printf ("this task does not exist !\n");
-                return;
+                printf ("\nthis task does not exist !\n");
+                continue;
                 }
                 num--;
 
@@ -188,15 +351,16 @@ void delete(task t[]) {
                 count--;
             } else if (ch == 2) {
                 count = 0;
-                printf ("<= all tasks deleted succefully ! =>\n");
+                printf ("\n<= all tasks deleted succefully ! =>\n");
             } else if (ch == 3) {
                 return;
-            } else printf ("invalid choice, try again !\n");
+            } else printf ("\ninvalid choice, try again !\n");
         } while (ch < 1 || ch > 3);
     }
 }
+
 void sort() {
-    printf ("test 5\n");
+    printf ("work in progress\n");
 }
 
 int main() {
@@ -204,7 +368,7 @@ int main() {
     task t[50];
 
     do {
-        printf ("=== Menu ===\n1-Add a new task\n2-Show tasks\n3-Edit a task\n4-Delete a task\n5-Sort tasks\n0-Exit\n");
+        printf ("\n=== Menu ===\n1-Add a new task\n2-Show tasks\n3-Edit a task\n4-Delete a task\n5-Sort tasks\n0-Exit\n");
 
         printf (" ==> entre your choice : ");
         scanf ("%d", &ch);
